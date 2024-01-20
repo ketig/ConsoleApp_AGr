ffb6087c455b4bb3a1b6221deca236125f338767
ketig <41895005+ketig@users.noreply.github.com>
2024-01-20 13:03:12 +0400

    Add project files.

---
+++ ConsoleApp_AGr/Program.cs
@@ -0,0 +1,21 @@
+﻿// pastebin.com/7QvYYzZv
+
+// დაბეჭდეთ 1-დან 100-ის ჩათვლით 3-ის ჯერადი
+for (int i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0) // 3-ის ჯერადი
+    {
+		Console.WriteLine(i);
+	}
+}
+
+// დაბეჭდეთ 1-დან 100-ის ჩათვლით ლუწი რიცხვები
+for (int i = 2; i <= 10; i += 2)
+{
+    Console.WriteLine(i);
+}
+
+
+// https://www.w3resource.com/csharp-exercises/array/index.php
+// 1. ... stores elements in an array and prints them.
+double[] myNumbers;
