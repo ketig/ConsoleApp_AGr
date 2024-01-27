90689e6f0e3451dd63a537525393d0b5eb29a6be
ketig <41895005+ketig@users.noreply.github.com>
2024-01-27 13:57:45 +0400

    array

--- ConsoleApp_AGr/Program.cs
+++ ConsoleApp_AGr/Program.cs
@@ -19,8 +19,8 @@ for (int i = 2; i <= 10; i += 2)
 // https://www.w3resource.com/csharp-exercises/array/index.php
 // 1. ... stores elements in an array and prints them.
 double[] myNumbers = { 1, 4, 3.5, 47 };
-string[] studNamesAgr = new string[4] { "Rati", "Mariami", "Gaga","Nika"};
-string[] studNamesAgr2 = new string[] { "Rati", "Mariami", "Gaga","Nika"};
+string[] studNamesAgr = new string[4] { "Rati", "Mariami", "Gaga", "Nika" };
+string[] studNamesAgr2 = new string[] { "Rati", "Mariami", "Gaga", "Nika" };
 
 double[] studentsAge = new double[9];
 
@@ -28,3 +28,31 @@ studentsAge[2] = 11;
 studentsAge[4] = 14;
 studentsAge[1] = 13;
 studentsAge[3] = 14;
+
+
+// string ტიპის მასივი
+string[] studentNames = {"Rati", "Goga", "Alexadnre", "Nika", "Demiko", 
+    "Giorgi", "Nikolozi"};
+
+
+
+string[] cars = new string[4]; // {"", "", "", ""}
+cars[1] = "Ford"; // {"", "Ford", "", ""}
+cars[2] = "Opel"; // {"", "Ford", "Opel", ""}
+cars[0] = "BMW"; // {"BMW", "Ford", "Opel", ""}
+cars[3] = "Mercedec"; // {"BMW", "Ford", "Opel", "Mercedec"}
+
+
+// დაბეჭდეთ მასივის ელემენტები თანმიმდევრობით
+// Console.WriteLine(cars[0]);
+// Console.WriteLine(cars[1]);
+// Console.WriteLine(cars[2]);
+// Console.WriteLine(cars[3]);
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine(cars[i]);
+}
+
+
+// დაბეჭდეთ მასივის ელემენტები უკუღმა
